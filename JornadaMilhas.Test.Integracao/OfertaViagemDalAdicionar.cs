a211163d4dbb494b0e1fc18e2dbe8f6af54bba53
Fernando Schulz <fasschulz@gmail.com>
2025-03-27 15:36:25 -0300

    Utilizando collection fixture para compartilhar recursos com outras classes

--- JornadaMilhas.Test.Integracao/OfertaViagemDalAdicionar.cs
+++ JornadaMilhas.Test.Integracao/OfertaViagemDalAdicionar.cs
@@ -6,7 +6,8 @@ using Xunit.Abstractions;
 
 namespace JornadaMilhas.Test.Integracao;
 
-public class OfertaViagemDalAdicionar : IClassFixture<ContextoFixture>
+[Collection(nameof(ContextoCollection))]
+public class OfertaViagemDalAdicionar
 {    
     private readonly JornadaMilhasContext _context;
 
---
+++ JornadaMilhas.Test.Integracao/OfertaViagemDalRecuperarPorId.cs
@@ -0,0 +1,32 @@
+﻿using JornadaMilhas.Dados;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace JornadaMilhas.Test.Integracao;
+
+[Collection(nameof(ContextoCollection))]
+public class OfertaViagemDalRecuperarPorId
+{
+    private JornadaMilhasContext _context;
+
+    public OfertaViagemDalRecuperarPorId(ITestOutputHelper output, ContextoFixture fixture)
+    {
+        _context = fixture.Context;
+        output.WriteLine(_context.GetHashCode().ToString());
+    }
+
+    [Fact]
+    public void RetornaNuloQuandoIdInexistente()
+    {
+        var dal = new OfertaViagemDAL(_context);
+
+        var ofertaRecuperada = dal.RecuperarPorId(-2);
+
+        Assert.Null(ofertaRecuperada);
+    }
+}
