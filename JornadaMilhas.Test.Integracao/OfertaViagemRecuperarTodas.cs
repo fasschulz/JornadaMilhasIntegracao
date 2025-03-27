58e87ae060eb215689874f6b9132bcbd6d1bb9f9
Fernando Schulz <fasschulz@gmail.com>
2025-03-27 15:51:43 -0300

    Teste OfertaViagem RecuperarTodas

---
+++ JornadaMilhas.Test.Integracao/OfertaViagemRecuperarTodas.cs
@@ -0,0 +1,33 @@
+﻿using JornadaMilhas.Dados;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace JornadaMilhas.Test.Integracao;
+
+[Collection(nameof(ContextoCollection))]
+public class OfertaViagemRecuperarTodas
+{
+    private JornadaMilhasContext _context;
+
+    public OfertaViagemRecuperarTodas(ITestOutputHelper output, ContextoFixture fixture)
+    {
+        _context = fixture.Context;
+        output.WriteLine(_context.GetHashCode().ToString());
+    }
+
+    [Fact]
+    public void RecuperaTodasAsOfertasCadastradas()
+    {
+        var dal = new OfertaViagemDAL(_context);
+        var ofertas = dal.RecuperarTodas();
+
+        Assert.NotNull(ofertas);
+        Assert.True(ofertas.Count() > 0);
+    }
+}
