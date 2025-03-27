7927d3265b6235b75a5eb86fe22aec7984e7c6e0
Fernando Schulz <fasschulz@gmail.com>
2025-03-27 14:23:35 -0300

    Setup OfertaViagemDalAdicionar

--- JornadaMilhas.Test.Integracao/OfertaViagemDalAdicionar.cs
+++ JornadaMilhas.Test.Integracao/OfertaViagemDalAdicionar.cs
@@ -7,6 +7,18 @@ namespace JornadaMilhas.Test.Integracao;
 
 public class OfertaViagemDalAdicionar
 {
+    private readonly DbContextOptions<JornadaMilhasContext> _options;
+    private readonly JornadaMilhasContext _context;
+
+    public OfertaViagemDalAdicionar()
+    {
+        _options = new DbContextOptionsBuilder<JornadaMilhasContext>()
+            .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=JornadaMilhas;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False")
+            .Options;
+
+        _context = new JornadaMilhasContext(_options);
+    }
+
     [Fact]
     public void RegistraOfertaNoBanco()
     {
@@ -14,19 +26,34 @@ public class OfertaViagemDalAdicionar
         Periodo periodo = new Periodo(new DateTime(2024, 8, 20), new DateTime(2024, 8, 30));
         double preco = 350;
 
-        var options = new DbContextOptionsBuilder<JornadaMilhasContext>()
-            .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=JornadaMilhas;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False")
-            .Options;
-
-        var context = new JornadaMilhasContext(options);
-
         var oferta = new OfertaViagem(rota, periodo, preco);
-        var dal = new OfertaViagemDAL(context);
+        var dal = new OfertaViagemDAL(_context);
 
         dal.Adicionar(oferta);
 
         var ofertaIncluida = dal.RecuperarPorId(oferta.Id);
         Assert.NotNull(ofertaIncluida);
         Assert.Equal(ofertaIncluida.Preco, oferta.Preco);
+    }
+
+    [Fact]
+    public void RegistraOfertaNoBancoComIformacoesCorretas()
+    {
+        Rota rota = new Rota("São Paulo", "Fortaleza");
+        Periodo periodo = new Periodo(new DateTime(2024, 8, 20), new DateTime(2024, 8, 30));
+        double preco = 350;
+
+        var oferta = new OfertaViagem(rota, periodo, preco);
+        var dal = new OfertaViagemDAL(_context);
+
+        dal.Adicionar(oferta);
+
+        var ofertaIncluida = dal.RecuperarPorId(oferta.Id);
+
+        Assert.Equal(ofertaIncluida.Rota.Origem, oferta.Rota.Origem);
+        Assert.Equal(ofertaIncluida.Rota.Destino, oferta.Rota.Destino);
+        Assert.Equal(ofertaIncluida.Periodo.DataInicial, oferta.Periodo.DataInicial);
+        Assert.Equal(ofertaIncluida.Periodo.DataFinal, oferta.Periodo.DataFinal);
+        Assert.Equal(ofertaIncluida.Preco, oferta.Preco, 0.001);
     }
 }
